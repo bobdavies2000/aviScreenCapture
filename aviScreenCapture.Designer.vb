85a5c4070f8fb0fd1002655e84b1564c49a39cdc
Bob Davies <bobdavies2000@gmail.com>
2020-11-29 22:05:56 -0800

    Add project files.

---
+++ aviScreenCapture.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class aviScreenCapture
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.fileName = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(34, 42)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(156, 49)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(286, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Start capturing the screen contents for:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(465, 43)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(576, 49)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Seconds"
+        '
+        'fileName
+        '
+        Me.fileName.AutoSize = True
+        Me.fileName.Location = New System.Drawing.Point(82, 94)
+        Me.fileName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.fileName.Name = "fileName"
+        Me.fileName.Size = New System.Drawing.Size(123, 20)
+        Me.fileName.TabIndex = 6
+        Me.fileName.Text = "output file name"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(36, 89)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(38, 32)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 152)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(203, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Capture frame count here..." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'aviScreenCapture
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(1200, 692)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.fileName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "aviScreenCapture"
+        Me.Text = "appCapture"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents fileName As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ aviScreenCapture.vb
@@ -0,0 +1,95 @@
+﻿Imports System.Threading
+Imports System.Runtime.InteropServices
+Imports System.IO
+Imports SharpAvi
+Imports SharpAvi.Codecs
+Imports SharpAvi.Output
+Public Class aviScreenCapture
+    Dim picCount As Integer
+    Dim myLock As New Mutex(True, "myLock")
+    Dim startTime As DateTime
+    Dim duration As Integer
+    Dim codec As FourCC
+    Dim screenThread As Thread
+    Dim threadStop As Boolean
+    Dim aviFileName As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If IsNumeric(TextBox1.Text) = False Then TextBox1.Text = "5"
+        If Button1.Text = "Start" Then
+            SaveSetting("aviScreenCapture", "duration", "duration", TextBox1.Text)
+            SaveSetting("aviScreenCapture", "filename", "filename", fileName.Text)
+
+            duration = CInt(TextBox1.Text)
+            startTime = Now()
+            Button1.Text = "Stop"
+            Timer1.Interval = 30
+            Timer1.Enabled = True
+
+            threadStop = False
+            screenThread = New Thread(AddressOf RecordScreen)
+            screenThread.Name = "RecordStream"
+            screenThread.IsBackground = True
+            screenThread.Start()
+        Else
+            threadStop = True
+            screenThread.Join()
+            Timer1.Enabled = False
+
+            Button1.Text = "Start"
+            Dim diff = Now().Subtract(startTime)
+            Me.Text = "Captured " + CStr(picCount) + " frames in " + Format(diff.TotalMilliseconds / 1000, "#0.0") + " seconds.  FPS = " + Format(picCount / diff.TotalSeconds, "#0.0")
+            Me.Width = Me.Width + 1
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Static lastPicCount = picCount
+        If lastPicCount = picCount Then Exit Sub
+        lastPicCount = picCount
+        If Label3.Height >= Me.Height - Label3.Top - 80 Then Label3.Text = ""
+        Label3.Text += vbCrLf + "Capturing frame " + CStr(picCount)
+        If Now().Subtract(startTime).Seconds > duration Then Button1_Click(sender, e)
+    End Sub
+    Private Sub appCapture_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        codec = KnownFourCCs.Codecs.MotionJpeg
+        Dim fileinfo As New FileInfo(Application.StartupPath + "/../../Video/sample.avi")
+        aviFileName = fileinfo.FullName
+        TextBox1.Text = GetSetting("aviScreenCapture", "duration", "duration", "1")
+        fileName.Text = GetSetting("aviScreenCapture", "filename", "filename", aviFileName)
+    End Sub
+    Private Sub RecordScreen()
+        Dim offset = 8
+        Dim top = Me.Top
+        Dim left = Me.Left + offset
+        Dim w = Width - 2 * offset
+        Dim h = Height - offset
+
+        Dim writer As AviWriter
+        Dim videoStream As IAviVideoStream
+        writer = New AviWriter(fileName.Text)
+        videoStream = writer.AddMotionJpegVideoStream(w, h, 70)
+        Dim buffer(w * h * 4) As Byte
+        Dim bmp = New Bitmap(w, h)
+        Dim videoWriteTask As Task = Nothing
+        While 1
+            Console.WriteLine("frame = " + CStr(picCount) + " w = " + CStr(w) + " h = " + CStr(h))
+            Application.DoEvents()
+
+            Dim g = Graphics.FromImage(bmp)
+            g.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+            g.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+            g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+            g.CopyFromScreen(New Point(left, top), New Point(0, 0), New Drawing.Size(w, h))
+            g.Flush()
+            Dim bits = bmp.LockBits(New Rectangle(0, 0, w, h), Imaging.ImageLockMode.ReadOnly, Imaging.PixelFormat.Format32bppPArgb)
+            Marshal.Copy(bits.Scan0, buffer, 0, buffer.Length)
+            bmp.UnlockBits(bits)
+
+            videoWriteTask?.Wait()
+            videoWriteTask = videoStream.WriteFrameAsync(True, buffer, 0, buffer.Length)
+            picCount += 1
+            If threadStop = True Then Exit While
+        End While
+        videoWriteTask?.Wait()
+        writer.Close()
+    End Sub
+End Class
